92bc5290cc828e16cc676be2dd11f6ea21cba911
Magesh Aramuthan <Magesh.Aramuthan@plateauinc.com>
2025-07-19 22:38:52 +0530

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext storeContext;
+
+        public ProductsController(StoreContext storeContext)
+        {
+            this.storeContext = storeContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await storeContext.Product.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await storeContext.Product.FindAsync(id);
+            if(product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(Product product)
+        {
+            storeContext.Product.Add(product);
+            await storeContext.SaveChangesAsync();
+            return Ok(product);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id && !IsProductExist(id))
+                return BadRequest("cannot update this product");
+            storeContext.Entry(product).State = EntityState.Modified;
+            await storeContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product =await storeContext.Product.FindAsync(id);
+            if (product == null) 
+                return BadRequest("cannot delete this project");
+            storeContext.Product.Remove(product);
+            await storeContext.SaveChangesAsync();
+            return NoContent();
+
+        }
+
+        private bool IsProductExist(int id)
+        {
+            return storeContext.Product.Any(x => x.Id == id);
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Product> Product { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+ 
